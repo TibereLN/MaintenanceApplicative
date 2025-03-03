9a93f0b44dc95064e614919b8105d992a1025131
TibereLN <tibere55190@gmail.com>
2025-03-03 11:06:50 +0100

    refacto de ouf 🤯 (crazy)

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -2,17 +2,17 @@ package com.gildedrose;
 
 class GildedRose {
     public static final String AGED_BRIE = "Aged Brie";
-    public static final String BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT = "Backstage passes to a DamYUNG concert";
     public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
     public static final String CONJURED = "Conjured";
-    ItemUpdate[] items;
+    ItemSimple[] items;
 
-    public GildedRose(ItemUpdate[] items) {
+    public GildedRose(ItemSimple[] items) {
         this.items = items;
     }
 
     public void updateQuality() {
-        for (ItemUpdate item : items) {
+        for (ItemSimple item : items) {
             item.updateQuality();
         }
     }
